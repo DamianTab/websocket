54b51b4776505a2a865bba5bc76fdf2d5b4faa21
DamianTab <dam.tab@wp.pl>
2021-06-09 13:53:40 +0200

    init project

---
+++ src/test/java/damian/tab/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package damian.tab.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
